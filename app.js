28787f81ca2c69f31806ce20c5d64c7a7009348b
Bookieblue <oluwabukola.fasan@gmail.com>
2023-10-25 20:59:14 +0100

    new update

--- app.js
+++ app.js
@@ -62,35 +62,6 @@ const prevButton = document.querySelector('.fa.fa-chevron-left');
 const nextButton = document.querySelector('.fa.fa-chevron-right');
 
 
-//form
-const form =document.querySelector('form');
-statusTxt = form.querySelector('.btn-con span');
-
-
-form.onsubmit =(e)=>{
-    e.preventDefault();
-    statusTxt.style.display = "block";
-    statusTxt.style.color= "#0D6EFD" ;
-    let xhr = new XMLHttpRequest(); //creating new xml object
-    xhr.open("POST", "contact.php", true); //sending post request to contact.php file
-    xhr.onload = () =>{ //once ajax loaded
-          if (xhr.readyState == 4 && xhr.status == 200){
-            let response = xhr.response;
-         if (response.indexOf("Email and message field is required!") != -1 || response.indexOf("Enter a valid email address!") || response.indexOf("sorry, failed to send your message!"))  {
-            statusTxt.style.color= "pink" ;
-         }
-         else{
-            form.reset();
-            setTimeout(()=>{
-                statusTxt.style.display = "none";
-            }, 3000);
-         }
-            statusTxt.innerText = response;
-          }
-    }
-    let formData = new FormData(form);
-    xhr.send(formData); //sending form data
-}
 
 
 
